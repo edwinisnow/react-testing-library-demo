f5518b1f043e1951462485d0d9e49ddf10ad990f
edwinisnow <edwin04@gmail.com>
2023-05-29 08:06:40 +0100

    describe

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  //   const textElement = screen.getByText(/hello/i);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    //   const textElement = screen.getByText(/hello/i);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Bob" />);
-  const textElement = screen.getByText("Hello Bob");
-  expect(textElement).toBeInTheDocument();
+  test("renders with a name", () => {
+    render(<Greet name="Bob" />);
+    const textElement = screen.getByText("Hello Bob");
+    expect(textElement).toBeInTheDocument();
+  });
 });
